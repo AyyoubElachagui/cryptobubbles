830b1635ae37d611f4752469328839f95381455b
AyyoubElachagui <best_way_development@outlook.fr>
2022-02-19 19:38:21 +0100

    test crypto animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,325 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Paint painter;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  DrawTriangleShape() {
+    painter = Paint()
+      ..color = Colors.purpleAccent
+      ..style = PaintingStyle.fill;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        child: Wrap(
+          children: [
+            CryptoWidget(
+              alignmentStart: Alignment.center,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    "BTC",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CryptoWidget(
+              alignmentStart: Alignment.bottomLeft,
+              child:
+                  CustomPaint(size: Size(40, 40), painter: DrawTriangleShape(),),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.bottomRight,
+              child:
+                  CustomPaint(size: Size(180, 180), painter: DrawTriangleShape()),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topRight,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    "ETH",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://img.nrj.fr/yivPvar6VZ2iaxP_Amf7X_64m8k=/800x450/smart/https%3A%2F%2Fimage-api.nrj.fr%2Fmedias%2F2019%2F03%2Fpicto-cherie-16_5c98e590268f5.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+
+            CryptoWidget(
+              alignmentStart: Alignment.center,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(1000),
+                    topLeft: Radius.circular(1000),
+                  )
+                ),
+                child: Center(
+                  child: Text(
+                    "BTC",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.bottomLeft,
+              child:
+              CustomPaint(size: Size(40, 40), painter: DrawTriangleShape(),),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.bottomRight,
+              child:
+              CustomPaint(size: Size(180, 180), painter: DrawTriangleShape()),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topRight,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    "ETH",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://img.nrj.fr/yivPvar6VZ2iaxP_Amf7X_64m8k=/800x450/smart/https%3A%2F%2Fimage-api.nrj.fr%2Fmedias%2F2019%2F03%2Fpicto-cherie-16_5c98e590268f5.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+
+
+            CryptoWidget(
+              alignmentStart: Alignment.center,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    "BTC",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.bottomLeft,
+              child:
+              CustomPaint(size: Size(40, 40), painter: DrawTriangleShape(),),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.bottomRight,
+              child:
+              CustomPaint(size: Size(180, 180), painter: DrawTriangleShape()),
+            ),
+            CryptoWidget(
+              alignmentStart: Alignment.topRight,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    "ETH",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            CryptoWidget(
+              alignmentStart: Alignment.topLeft,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  "https://img.nrj.fr/yivPvar6VZ2iaxP_Amf7X_64m8k=/800x450/smart/https%3A%2F%2Fimage-api.nrj.fr%2Fmedias%2F2019%2F03%2Fpicto-cherie-16_5c98e590268f5.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class CryptoWidget extends StatefulWidget {
+  final Alignment alignmentStart;
+  final Widget child;
+
+  CryptoWidget({Key? key, required this.alignmentStart, required this.child})
+      : super(key: key);
+
+  @override
+  _CryptoWidgetState createState() => _CryptoWidgetState();
+}
+
+class _CryptoWidgetState extends State<CryptoWidget> {
+  Alignment? _alignment;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      setState(() => _alignment = widget.alignmentStart);
+    });
+  }
+
+  void animation() {
+    Timer.periodic(new Duration(seconds: 2), (timer) {
+      debugPrint(timer.tick.toString());
+      switch (_alignment.toString()) {
+        case "Alignment.center":
+          setState(() => _alignment = Alignment.bottomLeft);
+          break;
+        case "Alignment.bottomLeft":
+          setState(() => _alignment = Alignment.topLeft);
+          break;
+        case "Alignment.topLeft":
+          setState(() => _alignment = Alignment.bottomRight);
+          break;
+        case "Alignment.bottomRight":
+          setState(() => _alignment = Alignment.topRight);
+          break;
+        case "Alignment.topRight":
+          setState(() => _alignment = Alignment.center);
+          _alignment = Alignment.center;
+      }
+    });
+  }
+
+  Widget _cryptoAnimation() {
+    animation();
+    return AnimatedContainer(
+      width: 80,
+      height: 80,
+      alignment: _alignment,
+      duration: Duration(seconds: 1),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+      ),
+      child: widget.child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _cryptoAnimation();
   }
 }
